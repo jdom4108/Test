1f7cc6c2b9643440d0aadf82cc72af79218ee5d9
jdom4108 <JuliDomi34@gmail.com>
2026-02-20 19:01:03 -0600

    Updated Code
    No

--- Test/Test/test.java
+++ Test/Test/test.java
@@ -1,13 +1,41 @@
-public final boolean nine = 2000;
+public class test {
 
-private boolean integer = randomInt(200);
+    public final int nine = 2000;
 
-MotorOn boolean(motorOn){
-    if(true){
-        randomInt(2,9);
-    }else{
-        push = 200;
+    private int integer 
+    public boolean motorOn(boolean motorOn) {
+        if (motorOn) {
+            randomInt(2, 9);
+            return true;
+        } else {
+            int push = 200;
+            
+            return false;
+        }
     }
-}
 
-//DONT CLOSE THIS WHOEVER IS READING THIS ON FRIDAY!!!!!!!!
+    @SuppressWarnings("unused")
+    public void explodeAndRun() {
+        onExplode();
+        runMotor();
+        int value = 300;
+    }
+
+    private static int randomInt(int bound) {
+        return (int) (Math.random() * bound);
+    }
+
+    private static int randomInt(int min, int max) {
+        return min + (int) (Math.random() * (max - min + 1));
+    }
+
+    private void onExplode() {
+    
+    }
+
+    private void runMotor() {
+
+    }
+
+    //DONT CLOSE THIS WHOEVER IS READING THIS ON FRIDAY!!!!!!!!
+}
