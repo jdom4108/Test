5dc3393a3e4b7cef5c70587f9fb2e6f7e0fe69bf
jdom4108 <JuliDomi34@gmail.com>
2026-02-20 18:39:51 -0600

    Added Test
    Hopefully this clears up VS code errors. 2/20/26.    20/2/26

---
+++ Test/Test/test.java
@@ -0,0 +1,13 @@
+public final boolean nine = 2000;
+
+private boolean integer = randomInt(200);
+
+MotorOn boolean(motorOn){
+    if(true){
+        randomInt(2,9);
+    }else{
+        push = 200;
+    }
+}
+
+//DONT CLOSE THIS WHOEVER IS READING THIS ON FRIDAY!!!!!!!!
